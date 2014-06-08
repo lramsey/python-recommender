b7abe29e6b635b964d2503fbfa4096baac334bb9
lramsey <lramsey177@gmail.com>
2014-06-08 12:20:42 -0700

    testing set up in script.js

--- routes/script.js
+++ routes/script.js
@@ -1,16 +1,19 @@
 /* GET script page. */
 results = [];
-names =  JSON.stringify(['bob','randy','steve']);
-mockNames = 'False';
-products = JSON.stringify(['starcraft','halflife','portal']);
-mockProducts = 'False';
-matrix = JSON.stringify([[1,1,1],[1,0,0],[0,1,1]]);
-mockMatrix = 'False';
+
+var mockNames = 300;
+var mockProducts = 100;
+var mockMatrix = 'False';
+
+var names =  JSON.stringify(['bob','randy','steve']);
+var products = JSON.stringify(['starcraft','halflife','portal']);
+var matrix = JSON.stringify([[1,1,1],[1,0,0],[0,1,1]]);
+
 exports.datum = function(req, res){
-  if(results.length === 0){
+  if(results.length > -1){
     var python = require('child_process').spawn(
       'python',
-    ["./pyscript/exec.py", /*mockNames*/names, /*mockProducts*/products, /*mockMatrix*/matrix]);
+    ["./pyscript/exec.py", mockNames, mockProducts, mockMatrix]);
     output = '';
     python.stdout.on('data', function(data){
       output += data;
